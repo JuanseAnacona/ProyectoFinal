d04bed50137d4149b3e1abcb2870c19e0087de42
JuanseAnacona <juananaconar@gmail.com>
2023-05-30 20:04:03 -0500

    Adicion del footer funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Footer from './assets/commons/Footer'
 import Header from './assets/commons/header'
+
 
 function App() {
     return (
 <div> 
 <Header/>
+<Footer/>
 </div>
+
 
     )
 }
---
+++ src/assets/commons/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles/Footer.css";
+
+
+
+
+
+const Footer = () => {
+    return (
+        <footer> 
+            <div className="Footer">
+            <ul>
+                <li className="nav-item">Contactanos</li>
+                <li className="nav-item">Noticias</li>
+                <li className="nav-item">Quienes somos</li>
+                <li className="nav-item"></li>
+                <li className="nav-item"></li>
+            </ul>
+            <p>
+                VideoJuegos 2019 Todos los derechos reservados
+            </p>
+           
+            </div>
+        
+        </footer>
+        
+    );
+}
+
+
+export default Footer;
