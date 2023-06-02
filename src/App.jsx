1b694bbb1e3aeb2ea40690f2f39f0aa1bb96cb80
JuanseAnacona <juananaconar@gmail.com>
2023-06-01 21:29:16 -0500

    Tratando de crear el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './assets/commons/header'
 import Footer from './assets/commons/Footer'
 import Navegador from './assets/commons/Navegador'
 import Home from './Pages/Home'
+import ProductList from './Pages/juegos'
 
 
 
