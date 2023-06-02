72f971dd00ad3872c2e505c40df516261ce3d947
ChrommeRL <andrewsanchezprog@gmail.com>
2023-06-01 19:24:47 -0500

    home updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,16 +9,18 @@ const Home = () => {
 
         <div className="slider">
             <ul>
-                <li><img src="https://i0.wp.com/www.gamerfocus.co/wp-content/uploads/2022/12/valorant-diciembre-2022-pase-gratis-evento-beta-swiftplay.jpg?w=1920&ssl=1" alt="" /></li>
-                <li><img src="src\assets\commons\styles\Banner 2.png" alt="" /></li>
-                <li><img src="src\assets\commons\styles\Banner 3.png" alt="" /></li>
+                <li><img src="src\assets\commons\styles\Banner.png" alt="1" /></li>
+                <li><img src="src\assets\commons\styles\Banner 2.png" alt="2" /></li>
+                <li><img src="src\assets\commons\styles\Banner 3.png" alt="3" /></li>
+                <li><img src="src\assets\commons\styles\Banner 4.png" alt="4" /></li>
             </ul>
         </div>
+
         <div className="Texto"> 
-        <h2>Bienvenido a Mega Games</h2>
+            <h2>Bienvenido a Mega Games</h2>
                 <p> Estamos encantados de recibirte en nuestro mundo virtual lleno de emoción y aventuras. Aquí encontrarás todo lo que necesitas para satisfacer tu pasión por los juegos. Ya sea que te gusten  de acción, estrategia, aventuras o deportes, tenemos una amplia selección de títulos para todas las plataformas. Navega por nuestras estanterías digitales y descubre los últimos lanzamientos, clásicos atemporales y títulos exclusivos que te llevarán a mundos imaginarios y desafiantes.</p>
 
-        </div>
+         </div>
         </section>
         
         </div>
